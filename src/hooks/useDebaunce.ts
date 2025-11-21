47509af52227b83eb35433bb2785f692a8142084
valik <valik.0404@mail.ru>
2025-11-21 23:00:49 +0300

    fix types in useDebaunce

--- src/hooks/useDebaunce.ts
+++ src/hooks/useDebaunce.ts
@@ -1,9 +1,7 @@
 import { useEffect, useState } from "react";
 
-type UseDebounceType = (value: string, delay?: number) => string;
-
-export const useDebounce: UseDebounceType = (value, delay = 300) => {
-  const [debauncedValue, setDebauncedValue] = useState(value);
+export function useDebounce<T>(value: T, delay = 300): T {
+  const [debauncedValue, setDebauncedValue] = useState<T>(value);
 
   useEffect(() => {
     const handler = setTimeout(() => {
